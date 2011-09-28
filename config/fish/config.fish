92a6211335d5fdc245956f84627ac5e922c03f0c
John Haitas <john@haitas.com>
2011-09-28 13:58:28 -0500

    temporary addition of macports path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -18,3 +18,5 @@ function fish_prompt
             printf '%s@%s %s%s%s> '  (whoami) (hostname|cut -d . -f 1) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
          end 
 end
+
+set PATH /opt/local/bin $PATH
